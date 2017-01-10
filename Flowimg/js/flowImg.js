a1380d67cc585d72ace1477184614c91b30c36b0
yinhuiyao <632212519@qq.com>
2017-01-11 02:25:04 +0800

    exchange img

--- Flowimg/js/flowImg.js
+++ Flowimg/js/flowImg.js
@@ -5,7 +5,7 @@ window.onload=function(){
   window.onscroll=function(){
     if(checkFlag()){
     //  console.log("scroll");
-    var imgData={"data":[{"src":"1.jpg"},{"src":"3.jpg"},{"src":"6.jpg"},{"src":"7.jpg"}]};
+    var imgData={"data":[{"src":"1.jpg"},{"src":"2.jpg"},{"src":"3.jpg"},{"src":"4.jpg"},{"src":"5.jpg"},{"src":"6.jpg"},{"src":"7.jpg"},{"src":"8.jpg"},{"src":"9.jpg"},{"src":"10.jpg"}]};
 
       var contain=document.getElementById("container");
       for(var i=0;i<imgData.data.length;i++){
--- FlowimgJQuery/FlowImg.js
+++ FlowimgJQuery/FlowImg.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     $(window).on("load",function(){
         imgLocation();
-        var imgData={"data":[{"src":"1.jpg"},{"src":"3.jpg"},{"src":"6.jpg"},{"src":"7.jpg"}]};
+    var imgData={"data":[{"src":"1.jpg"},{"src":"2.jpg"},{"src":"3.jpg"},{"src":"4.jpg"},{"src":"5.jpg"},{"src":"6.jpg"},{"src":"7.jpg"},{"src":"8.jpg"},{"src":"9.jpg"},{"src":"10.jpg"}]};
         window.onscroll=function(){
             if(scrollSide()){
                 $.each(imgData.data,function(index,value){
